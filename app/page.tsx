e4b6442a677bfd073a46b8c1b21e35d695d10a14
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-05-07 21:30:19 +0200

    uncomment new lines

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ const Page = async () => {
         <main className="flex flex-col gap-8 sm:gap-16">
             Optimize your images!
             <Uploader />
-            <Image src="/api/image/8mtsvc" width={512} height={512} alt={'asd'} />
-            {/* <Image src="/api/image/8mtsvc/optimize" width={512} height={512} alt={'asd'} /> */}
+            <img src="/api/image/8mtsvc" width={512} height={512} alt={'asd'} />
+            <img src="/api/image/8mtsvc/optimize" width={512} height={512} alt={'asd'} />
             {/* <div className="flex flex-wrap gap-4">
                 {images.map((image) => (
                     <img key={image} src={image} alt="" className="w-32 h-32 object-cover" />
