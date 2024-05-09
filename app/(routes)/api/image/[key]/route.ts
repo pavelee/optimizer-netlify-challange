f3b01baee9b3241164312d3d58c111bf00ec1163
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-05-09 21:58:53 +0200

    feat: Optimize image loading with lazy loading and BlobStore integration
    The code changes in `app/(routes)/api/image/[key]/route.ts` introduce lazy loading for images on the page by integrating with the `BlobStore` from the `_config/store` module. This optimization improves page load performance by loading images only when they are visible on the screen. The `GET` function now retrieves the image blob using the `BlobStore.get` method and returns it as a response. This change enhances the user experience by reducing the initial page load time and improving the overall performance of image loading.

--- app/(routes)/api/image/[key]/route.ts
+++ app/(routes)/api/image/[key]/route.ts
@@ -1,3 +1,4 @@
+import { BlobStore } from 'app/_config/store';
 import { getImage } from 'app/actions/getImage';
 import { store } from 'app/utils/store';
 import { cookies } from 'next/headers';
@@ -12,19 +13,6 @@ type props = {
 export const GET = async (request: NextRequest, props: props) => {
     const cookie = cookies();
     const { key } = props.params;
-    const blob = await store().get(key, {
-        type: 'blob'
-    });
-
-    console.log(blob);
-    const b = new Blob([blob], { type: 'image/png' });
-    console.log(b);
-
-    return new NextResponse(b);
-
-    return new Response(blob, {
-        headers: {
-            'Content-Type': 'image/png'
-        }
-    });
+    const blob = await BlobStore.get(key);
+    return new NextResponse(blob);
 };
