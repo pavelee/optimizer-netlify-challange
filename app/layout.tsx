815e82fe58b1c041dbae248ebddf8be55323178b
Paweł Ciosek <pciosek@wip.pl>
2024-05-11 12:12:02 +0200

    feat: Introduce lazy loading for images and integrate with BlobStore
    The code changes optimize image loading on the page by introducing lazy loading for images. This enhancement improves page load performance by loading images only when they are visible on the screen. Additionally, the code integrates with the BlobStore to retrieve image blobs. These changes enhance the user experience by reducing the initial page load time and improving the overall performance of image loading.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 export const metadata = {
     title: {
@@ -15,15 +16,17 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
-                        <div className="grow">{children}</div>
-                        <Footer />
+            <AntdRegistry>
+                <body className="antialiased">
+                    <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
+                        <div className="flex flex-col w-full max-w-5xl mx-auto grow">
+                            <Header />
+                            <div className="grow">{children}</div>
+                            <Footer />
+                        </div>
                     </div>
-                </div>
-            </body>
+                </body>
+            </AntdRegistry>
         </html>
     );
 }
