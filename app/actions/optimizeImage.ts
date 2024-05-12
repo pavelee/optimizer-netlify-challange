718bb1771a9b4ce25b7c88b351ab64478d572a7d
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-05-12 15:33:40 +0200

    Fixed to type file extension we want to optimize

--- app/actions/optimizeImage.ts
+++ app/actions/optimizeImage.ts
@@ -1,9 +1,12 @@
 'use server';
 
+import { AssetRepository } from 'app/repository/AssetRepository';
 import { headers } from 'next/headers';
 
 export const optimizeImage = async (key: string) => {
     const head = headers();
+    const ar = new AssetRepository();
+    const image = await ar.findFile(key);
     const host = head.get('host');
     const isProd = process.env.NODE_ENV === 'production';
     let protocol = 'http';
@@ -11,7 +14,8 @@ export const optimizeImage = async (key: string) => {
         protocol = 'https';
     }
     const url = `/api/image/${key}`;
-    const optimizedUrl = `${protocol}://${host}/.netlify/images?url=${encodeURIComponent(url)}&q=50`;
+    const format = image?.getExtension();
+    const optimizedUrl = `${protocol}://${host}/.netlify/images?url=${encodeURIComponent(url)}&q=50&fm=${format}`;
 
     const fetchResponse = await fetch(optimizedUrl);
 
