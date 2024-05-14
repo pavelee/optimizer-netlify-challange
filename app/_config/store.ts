2c3781a79586dcf359250b94910e1ca089ae1ead
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-05-14 21:25:34 +0200

    Refactor BlobStore and JsonStore variables in store.ts

--- app/_config/store.ts
+++ app/_config/store.ts
@@ -2,6 +2,6 @@ import { AssetGroupDto } from 'app/dto/AssetGroupDto';
 import { AssetDTO } from 'app/models/Asset';
 import { NetlifyBlobStore, NetlifyJsonStore } from 'app/utils/store/NetlifyStore';
 
-export const BlobStore = new NetlifyBlobStore('images');
-export const JsonStore = new NetlifyJsonStore<AssetDTO>('data');
+export const BlobStore = new NetlifyBlobStore('images1');
+export const JsonStore = new NetlifyJsonStore<AssetDTO>('data1');
 export const AssetGroupStore = new NetlifyJsonStore<AssetGroupDto>('assetGroups2');
